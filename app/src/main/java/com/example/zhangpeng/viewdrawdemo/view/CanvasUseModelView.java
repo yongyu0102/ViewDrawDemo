9071761260bb0591632c80ef6b2fa3ddc6e5eca5
pengzhang <530198309@qq.com>
2016-12-13 00:12:08 +0800

    canvas

--- app/src/main/java/com/example/zhangpeng/viewdrawdemo/view/CanvasUseModelView.java
+++ app/src/main/java/com/example/zhangpeng/viewdrawdemo/view/CanvasUseModelView.java
@@ -47,20 +47,20 @@ public class CanvasUseModelView extends View {
 
         rect100 = new Rect(100, 100, 200, 200);
 
-        mPaint200=new Paint();
+        mPaint200 = new Paint();
         mPaint200.setColor(Color.RED);
-        rect200=new Rect(200,200,400,400);
+        rect200 = new Rect(200, 200, 400, 400);
 
-        mPaint00=new Paint();
+        mPaint00 = new Paint();
         mPaint00.setColor(Color.GREEN);
 
-        mPaint80=new Paint();
+        mPaint80 = new Paint();
         mPaint80.setColor(Color.YELLOW);
 
-        mPaint=new Paint();
-        rect=new Rect();
+        mPaint = new Paint();
+        mPaint.setColor(Color.BLACK);
+        rect = new Rect();
     }
-
 
 
     /**
@@ -68,6 +68,7 @@ public class CanvasUseModelView extends View {
      * 这里指定当使用该自定义的 View 时候，使用了 Wrap_content 属性后
      * 该 View  的宽和高都为 200dp ，这个尺寸需要根据具体需要和情况进行计算
      * 这里只是为了解释这个原理，任意给定了一个值而已
+     *
      * @param widthMeasureSpec
      * @param heightMeasureSpec
      */
@@ -75,117 +76,153 @@ public class CanvasUseModelView extends View {
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         // wrap_content 模式下给定宽/高质
-        int width=200;
-        int height=200;
-        int widthMode=MeasureSpec.getMode(widthMeasureSpec);
-        int heightMode=MeasureSpec.getMode(heightMeasureSpec);
-        int withSize=MeasureSpec.getSize(widthMeasureSpec);
-        int heightSize=MeasureSpec.getSize(heightMeasureSpec);
-        if(widthMode==MeasureSpec.AT_MOST&&heightMode==MeasureSpec.AT_MOST){
+        int width = 200;
+        int height = 200;
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        int withSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+        if (widthMode == MeasureSpec.AT_MOST && heightMode == MeasureSpec.AT_MOST) {
             //宽和高都为 wap_content 模式，进行设定值
-            setMeasuredDimension(width,height);
-        }else if(widthMode==MeasureSpec.AT_MOST){
+            setMeasuredDimension(width, height);
+        } else if (widthMode == MeasureSpec.AT_MOST) {
             //如果只有宽为 wrap_content 模式
-            setMeasuredDimension(width,heightSize);
-        }else if(heightMode==MeasureSpec.AT_MOST){
+            setMeasuredDimension(width, heightSize);
+        } else if (heightMode == MeasureSpec.AT_MOST) {
             //如果只有高为 wrap_content 模式
-            setMeasuredDimension(withSize,height);
+            setMeasuredDimension(withSize, height);
         }
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-//        translateTest(canvas);
-
-//        scaleTest01(canvas);
-
-//        scaleTest02(canvas);
-
-//        scaleTest03(canvas);
-
-// 将坐标系原点移动到画布正中心
+        // 将坐标系原点移动到画布正中心
         canvas.translate(getWidth() / 2, getHeight() / 2);
 
-        rect.set(0,-400,400,0);   // 矩形区域
+        rect.set(0, -400, 400, 0);   // 矩形区域
 
         mPaint.setColor(Color.BLACK);           // 绘制黑色矩形
         canvas.drawRect(rect,mPaint);
 
-        // 画布缩放  <-- 缩放中心向右偏移了200个单位
-        //即坐标轴向右偏移了 200 个单位
-        canvas.scale(-0.5f,-0.5f,200,0);
+        canvas.rotate(180,200,0);               // 旋转180度 <-- 旋转中心向右偏移200个单位
 
         mPaint.setColor(Color.BLUE);            // 绘制蓝色矩形
         canvas.drawRect(rect,mPaint);
+    }
 
+    private void rotateTest01(Canvas canvas) {
+        // 将坐标系原点移动到画布正中心
+        canvas.translate(getWidth() / 2, getHeight() / 2);
+
+        rect.set(0, -400, 400, 0);   // 矩形区域
+
+        mPaint.setColor(Color.BLACK);           // 绘制黑色矩形
+        canvas.drawRect(rect,mPaint);
+
+        canvas.rotate(180);                     // 旋转180度 <-- 默认旋转中心为原点
+
+        mPaint.setColor(Color.BLUE);            // 绘制蓝色矩形
+        canvas.drawRect(rect,mPaint);
+    }
+
+    private void scaleTest05(Canvas canvas) {
+        // 将坐标系原点移动到画布正中心
+        canvas.translate(getWidth() / 2, getHeight() / 2);
+
+        rect.set(-400, -400, 400, 400);   // 矩形区域
+        mPaint.setStyle(Paint.Style.STROKE);
+        for (int i = 0; i <= 20; i++) {
+            canvas.scale(0.8f, 0.8f);
+            canvas.drawRect(rect, mPaint);
+        }
+    }
+
+    private void scaleTest04(Canvas canvas) {
+        // 将坐标系原点移动到画布正中心
+        canvas.translate(getWidth() / 2, getHeight() / 2);
+
+        rect.set(0, -400, 400, 0);   // 矩形区域
+
+        mPaint.setColor(Color.BLACK);           // 绘制黑色矩形
+        canvas.drawRect(rect, mPaint);
+
+        // 画布缩放  <-- 缩放中心向右偏移了200个单位
+        //即坐标轴向右偏移了 200 个单位
+        canvas.scale(-0.5f, -0.5f, 200, 0);
+
+        mPaint.setColor(Color.BLUE);            // 绘制蓝色矩形
+        canvas.drawRect(rect, mPaint);
     }
 
     private void scaleTest03(Canvas canvas) {
         // 将坐标系原点移动到画布正中心
         canvas.translate(getWidth() / 2, getHeight() / 2);
 
-        rect.set(0,-400,400,0);   // 矩形区域
+        rect.set(0, -400, 400, 0);   // 矩形区域
 
         mPaint.setColor(Color.BLACK);           // 绘制黑色矩形
-        canvas.drawRect(rect,mPaint);
+        canvas.drawRect(rect, mPaint);
 
-        canvas.scale(-0.5f,-0.5f);          // 画布缩放
+        canvas.scale(-0.5f, -0.5f);          // 画布缩放
 
         mPaint.setColor(Color.BLUE);            // 绘制蓝色矩形
-        canvas.drawRect(rect,mPaint);
+        canvas.drawRect(rect, mPaint);
     }
 
     /**
      * 指定特定坐标点进行缩放
+     *
      * @param canvas
      */
     private void scaleTest02(Canvas canvas) {
         // 将坐标系原点移动到画布正中心
         canvas.translate(getWidth() / 2, getHeight() / 2);
 
-        rect100.set(0,-400,400,0);   // 矩形区域
+        rect100.set(0, -400, 400, 0);   // 矩形区域
 
         mPaint00.setColor(Color.BLACK);           // 绘制黑色矩形
-        canvas.drawRect(rect100,mPaint00);
+        canvas.drawRect(rect100, mPaint00);
 
         //以坐标点（200,0）为坐标点进行缩放
-        canvas.scale(0.5f,0.5f,200,0);          // 画布缩放  <-- 缩放中心向右偏移了200个单位
+        canvas.scale(0.5f, 0.5f, 200, 0);          // 画布缩放  <-- 缩放中心向右偏移了200个单位
 
         mPaint00.setColor(Color.BLUE);            // 绘制蓝色矩形
-        canvas.drawRect(rect100,mPaint00);
+        canvas.drawRect(rect100, mPaint00);
     }
 
     /**
      * 以默认坐标点进行缩放
+     *
      * @param canvas
      */
     private void scaleTest01(Canvas canvas) {
         //将画布坐标原点移动到整个 view 的中心位置
-        canvas.translate(getWidth()/2,getHeight()/2);
-        rect100.set(0,-400,400,0);
+        canvas.translate(getWidth() / 2, getHeight() / 2);
+        rect100.set(0, -400, 400, 0);
         mPaint00.setColor(Color.BLACK);           // 绘制黑色矩形
-        canvas.drawRect(rect100,mPaint00);
+        canvas.drawRect(rect100, mPaint00);
 
-        canvas.scale(0.5f,0.5f);                // 画布缩放
+        canvas.scale(0.5f, 0.5f);                // 画布缩放
 
         mPaint00.setColor(Color.BLUE);            // 绘制蓝色矩形
-        canvas.drawRect(rect100,mPaint00);
+        canvas.drawRect(rect100, mPaint00);
     }
 
     /**
      * 平移示例
+     *
      * @param canvas
      */
     private void translateTest(Canvas canvas) {
-        canvas.drawCircle(0,0,100,mPaint00);
+        canvas.drawCircle(0, 0, 100, mPaint00);
 
         mPaint00.setColor(Color.RED);
-        canvas.translate(200,200);
-        canvas.drawCircle(0,0,100,mPaint00);
+        canvas.translate(200, 200);
+        canvas.drawCircle(0, 0, 100, mPaint00);
 
         mPaint00.setColor(Color.BLACK);
-        canvas.translate(200,200);
-        canvas.drawCircle(0,0,100,mPaint00);
+        canvas.translate(200, 200);
+        canvas.drawCircle(0, 0, 100, mPaint00);
     }
 }
